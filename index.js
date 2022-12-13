b3df5cf6008a22e05079f84175501ad8769704de
amoubasher <amoubasher@gmail.com>
2022-12-13 14:54:27 -0800

    Changed readme and some comments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require('inquirer')
 const generateMarkdown = require('./utils/generateMarkdown')
 const fs = require('fs')
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -62,12 +62,12 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFileSync(fileName, data)
 }
 
-// TODO: Create a function to initialize app
+// Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
         .then(response => {
